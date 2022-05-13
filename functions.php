8eb2fea038b54205edfc62d2104ebfe7c7418e9e
user <97613444+Chamalui@users.noreply.github.com>
2022-05-13 15:50:33 +0300

    task 4 and 5

---
+++ functions.php
@@ -0,0 +1,82 @@
+<?php
+$is_auth = rand(0, 1);
+
+$catArray = [
+    ["category" => "Доски и лыжи", "image" => "boards"],
+    ["category" => "Крепления", "image" => "attachment"],
+    ["category" => "Ботинки", "image" => "boots"],
+    ["category" => "Одежда", "image" => "clothing"],
+    ["category" => "Инструменты", "image" => "tools"],
+    ["category" => "Разное", "image" => "other"]
+];
+$cat = [
+    [
+        "name" => "2014 Rossignol District Snowboard",
+        "category" => $catArray[0]["category"],
+        "cost" => 10999,
+        "img" => "img/lot-1.jpg"
+    ],
+    [
+        "name" => "DC Ply Mens 2016/2017 Snowboard",
+        "category" => $catArray[0]["category"],
+        "cost" => 15999,
+        "img" => "img/lot-2.jpg"
+    ],
+    [
+        "name" => "Крепления Union Contact Pro 2015 года размер L/XL",
+        "category" => $catArray[1]["category"],
+        "cost" => 8000,
+        "img" => "img/lot-3.jpg"
+    ],
+    [
+        "name" => "Ботинки для сноуборда DC Mutiny Charocal",
+        "category" => $catArray[2]["category"],
+        "cost" => 10999,
+        "img" => "img/lot-4.jpg"
+    ],
+    [
+        "name" => "Куртка для сноуборда DC Mutiny Charocal",
+        "category" => $catArray[3]["category"],
+        "cost" => 7500,
+        "img" => "img/lot-5.jpg"
+    ],
+    [
+        "name" => "Маска Oakley Canopy",
+        "category" => $catArray[5]["category"],
+        "cost" => 5400,
+        "img" => "img/lot-6.jpg"
+    ]
+];
+
+function money_format($number){
+    $number = ceil($number);
+    if($number>1000){
+        $result = number_format($number,0,","," ");
+    } else{
+        $result = $number;
+    }
+    return $result.'<b class="rub">p</b>';
+}
+
+function data_timer(){
+    $now_time = new DateTime('now');
+    $not_now_time = new DateTime('24:00');
+    $time_interval = $now_time->diff($not_now_time);
+    return $time_interval->format('%H:%i:%s');
+}
+
+function include_template($name,$data){
+    $name = 'templates/'.$name;
+    $result='';
+    if(!file_exists($name)){
+        return $result;
+    }
+    ob_start();
+    extract($data);
+    require ($name);
+    $result = ob_get_clean();
+    return $result;
+}
+
+$user_name = 'Alex'; // укажите здесь ваше имя
+?>
---
+++ templates/index.php
@@ -0,0 +1,49 @@
+<section class="promo">
+    <h2 class="promo__title">Нужен стафф для катки?</h2>
+    <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
+    <ul class="promo__list">
+        <?php
+        foreach ($catArray as $category)
+        {
+            ?>
+            <li class="promo__item promo__item--<?=$category["image"]?>">
+                <a class="promo__link" href="pages/all-lots.html"><?=$category["category"]?></a>
+            </li>
+            <?php
+        }
+        ?>
+    </ul>
+</section>
+<section class="lots">
+    <div class="lots__header">
+        <h2>Открытые лоты</h2>
+    </div>
+    <ul class="lots__list">
+        <?php
+        foreach ($cat as $goods)
+        {
+            ?>
+            <!--заполните этот список из массива с товарами-->
+            <li class="lots__item lot">
+                <div class="lot__image">
+                    <img src="<?=$goods["img"]?>" width="350" height="260" alt="">
+                </div>
+                <div class="lot__info">
+                    <span class="lot__category"><?=$goods["category"]?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$goods["name"]?></a></h3>
+                    <div class="lot__state">
+                        <div class="lot__rate">
+                            <span class="lot__amount">Стартовая цена</span>
+                            <span class="lot__cost"><?=money_format($goods["cost"])?></span>
+                        </div>
+                        <div class="lot__timer timer">
+                            <?=data_timer()?>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <?php
+        }
+        ?>
+    </ul>
+</section>
