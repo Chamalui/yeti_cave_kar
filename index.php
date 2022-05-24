9ef5a3d133c3643bec0e014be6713f1f11c6d73c
user <97613444+Chamalui@users.noreply.github.com>
2022-05-24 15:52:35 +0300

    task 9 (finished)

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 require_once ('functions.php');
 require_once ('connect.php');
 
-$page = include_template('index.php',['catArray'=>$catArray, 'lotsArray'=>$lotArray]);
-$layout_content = include_template('layout.php', ['page'=>$page, 'catArray'=>$catArray,'title'=>'Главная страница','is_auth'=>$is_auth,'user_name'=>$user_name, 'lotsArray'=>$lotsArray]);
+$page = include_template('index.php',['catArray'=>$catArray, 'lotArray'=>$lotArray]);
+$layout_content = include_template('layout.php', ['page'=>$page, 'catArray'=>$catArray,'title'=>'Главная страница','is_auth'=>$is_auth,'user_name'=>$user_name, 'lotArray'=>$lotArray]);
 print($layout_content);
 
 ?>
--- lot.php
+++ lot.php
@@ -3,22 +3,33 @@
 require_once ('functions.php');
 require_once ('connect.php');
 
-
 if(isset($_GET['id'])){
     $id = intval($_GET['id']);
+    print_r($id);
     foreach ($lotArray as $goods) {
         if($goods['id_lot']==$id){
-            $content = include_template('lot.php', ['lotArray' => $lotArray, 'betArray' => $betArray, 'userArray' => $userAray, 'category' => $category]);
-            $layout_content = include_template('layout.php', ['content' => $content, 'lotArray' => $lotArray, 'betArray' => $betArray, 'userArray' => $userAray, 'category' => $category]);
-
-            print($layout_content);
+            print_r($goods['id_lot']);
+            $currentLot=$goods;
+            /*$content = include_template('lot.php', ['currentLot' => $currentLot, 'userArray' => $userAray, 'catArray' => $catArray]);
+            $layout_content = include_template('layout.php', ['page' => $content, 'currentLot' => $currentLot, 'betArray' => $betArray, 'userArray' => $userAray, 'catArray' => $catArray, 'title' => $currentLot['name'], 'is_auth' => $is_auth, 'user_name' => $user_name]);*/
         }
+        /*print_r($currentLot);*/
     }
-    if(empty($lotArray['id_lot'][$id])){
+    /*if(empty($lotArray['id_lot'][$id])){
         $content = include_template('lot.php', ['lotArray' => $lotArray, 'betArray' => $betArray, 'userArray' => $userAray, 'category' => $category]);
         $layout_content = include_template('layout.php', ['content' => $content, 'lotArray' => $lotArray, 'betArray' => $betArray, 'userArray' => $userAray, 'category' => $category]);
 
         print($layout_content);
-    }
+    }*/
 /*добавить 404 как появится то при обовлении будет 404*/
+
 }
+if(!empty($lotArray['id_lot'][$id])){
+    $content = include_template('error404.php', ['currentLot' => $currentLot, 'userArray' => $userAray, 'catArray' => $catArray]);
+    $layout_content = include_template('layout.php', ['page' => $content, 'currentLot' => $currentLot, 'betArray' => $betArray, 'userArray' => $userAray, 'catArray' => $catArray, 'title' => $currentLot['name'], 'is_auth' => $is_auth, 'user_name' => $user_name]);
+}else{
+    $content = include_template('lot.php', ['currentLot' => $currentLot, 'userArray' => $userAray, 'catArray' => $catArray]);
+    $layout_content = include_template('layout.php', ['page' => $content, 'currentLot' => $currentLot, 'betArray' => $betArray, 'userArray' => $userAray, 'catArray' => $catArray, 'title' => $currentLot['name'], 'is_auth' => $is_auth, 'user_name' => $user_name]);
+}
+
+print($layout_content);
